166dfc6ff715727c45ce2146b35cbbfa7acd1876
Bob Haslett <bob.haslett@ft.com>
2016-05-12 18:34:51 +0100

    Add save png to line

--- wrapper-waterfall/chart.js
+++ wrapper-waterfall/chart.js
@@ -20,7 +20,9 @@ function makeChart(data, stylename, media, chartpadding,legend, yAlign,yHighligh
         return (d.name === media);
       });
     margin=margin[0].margin[0]
-    var colours=stylename.linecolours;
+    var colours=d3.scale.ordinal()
+        .range(stylename.linecolours);
+
     var plotWidth = w-(margin.left+margin.right);
     var plotHeight = h-(margin.top+margin.bottom);
     
@@ -28,93 +30,32 @@ function makeChart(data, stylename, media, chartpadding,legend, yAlign,yHighligh
     // console.log(margin)
     //you now have a chart area, inner margin data and colour palette - with titles pre-rendered
 
-    var yScale = d3.scale.linear()
-        .range([plotHeight, 0]);
+    var xScale = d3.scale.ordinal()
+        .rangeRoundBands([0, plotWidth], .3);
 
-    var groups = d3.keys(data[0]).filter(function(key) {
-        return key !== "group";
-    })
-
-
-    var xScale0 = d3.scale.ordinal()
-    .rangeRoundBands([0, plotWidth], 0, .3);
-
-    var xScale1 = d3.scale.ordinal();
+    var y = d3.scale.linear()
+        .range([plotHeight, 0]); 
 
     var xAxis = d3.svg.axis()
-    .scale(xScale0)
-    .orient("bottom");
-     data.forEach(function(d) {
-        //first calculate the starting height for each group
-        total = 0
-        totals = {}
-        for (var i=0; i<groups.length; i++) {
-          if (i == 3) {
-            total = total - +d[groups[i]];
-            totals[groups[i]] = total;
-          }
-          else {
-            totals[groups[i]] = total;
-            total += +d[groups[i]];
-          }
-        }
-        //then map category name, value, and starting height to each observation
-        d.formatted = groups.map(function(group) {
-          return {
-            name: group,
-            value: +d[group],
-            baseHeight: +totals[group]
-          };
-        });
-      });
-
-    xScale0.domain(data.map(function(d) { return d.group; }));
-    xScale1.domain(groups).rangeRoundBands([0, xScale0.rangeBand()]);
-    yScale.domain([0, d3.max(data, function(d) {
-        return d3.max(d.formatted, function(d) {
-            return d.value + d.baseHeight;
-        });
-    })]);
-
+        .scale(xScale)
+        .orient("bottom");
 
     var yAxis = d3.svg.axis()
-    .scale(yScale)
-    .orient(yAlign);
+        .scale(y)
+        .orient(yAlign)
 
-    var yLabel=plot.append("g")
-      .attr("class", media+"yAxis")
-      .call(yAxis);
+    //function to find all the positive values
+    var positive_val = data.filter(function(d) { return d.value > 0; });
+    console.log(JSON.stringify(positive_val));
 
-    //calculate what the ticksize should be now that the text for the labels has been drawn
-    var yLabelOffset=yLabel.node().getBBox().width
-    var yticksize=colculateTicksize(yAlign, yLabelOffset);
+    //function to calculate the sum of all the positive values
+    var maxSum = positive_val.reduce(function(sum, d) {
+       return sum + d.value;}, 0);
+    console.log("The maximum sum is "+maxSum);
 
-    yLabel.call(yAxis.tickSize(yticksize))
-    yLabel
-        .attr("transform",function(){
-            if (yAlign=="right"){
-                return "translate("+(margin.left)+","+margin.top+")"
-            }
-            else return "translate("+(w-margin.right)+","+margin.top+")"
-            })
-
-    //identify 0 line if there is one
-    var originValue = 0;
-    var origin = plot.selectAll(".tick").filter(function(d, i) {
-            return d==originValue || d==yHighlight;
-        })
-    .classed(media+"origin",true);
-
-    var xLabel=plot.append("g")
-      .attr("class", media+"xAxis")
-      .attr("transform",function(){
-                return "translate("+(margin.left)+","+plotHeight+")"
-            })
-      .call(xAxis);
-
-
-
-
+    //to calculate the new Domain by adding 120 
+    var yaxisRange=maxSum+120;
+    console.log("The y axis sum is "+yaxisRange);
 
 
 
