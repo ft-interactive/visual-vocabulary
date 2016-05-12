b66c29d05e6e6de15eb3ce92190b02ab46d6cd1e
Bob Haslett <bob.haslett@ft.com>
2016-05-12 16:41:41 +0100

    Add yAxis correctly positioned with origin highlight

---
+++ wrapper-waterfall/chart.js
@@ -0,0 +1,77 @@
+
+function makeChart(data, stylename, media, chartpadding,legend, yAlign,yHighlight){
+
+    var titleYoffset = d3.select("#"+media+"Title").node().getBBox().height
+    var subtitleYoffset=d3.select("#"+media+"Subtitle").node().getBBox().height;
+
+    // return the series names from the first row of the spreadsheet
+    var seriesNames = Object.keys(data[0]).filter(function(d){ return d != 'date'; });
+    //Select the plot space in the frame from which to take measurements
+    var frame=d3.select("#"+media+"chart")
+    var plot=d3.select("#"+media+"plot")
+
+    var yOffset=d3.select("#"+media+"Subtitle").style("font-size");
+    yOffset=Number(yOffset.replace(/[^\d.-]/g, ''));
+    
+    //Get the width,height and the marginins unique to this chart
+    var w=plot.node().getBBox().width;
+    var h=plot.node().getBBox().height;
+    var margin=chartpadding.filter(function(d){
+        return (d.name === media);
+      });
+    margin=margin[0].margin[0]
+    var colours=stylename.linecolours;
+    var plotWidth = w-(margin.left+margin.right);
+    var plotHeight = h-(margin.top+margin.bottom);
+    
+    // console.log(plotWidth,colours,plotHeight,data)
+    // console.log(margin)
+    //you now have a chart area, inner margin data and colour palette - with titles pre-rendered
+
+    var yScale = d3.scale.linear()
+        .range([plotHeight, 0]);
+
+    var min=d3.min(data, function(d) { return +d.value;})
+    var max=d3.max(data, function(d) { return +d.value;})
+
+    //var max=d3.max(data, function(d,i) { return +d.value;});
+    yScale.domain([0, 300]);
+
+    var yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient(yAlign);
+
+    var yLabel=plot.append("g")
+      .attr("class", media+"yAxis")
+      .call(yAxis);
+
+    //calculate what the ticksize should be now that the text for the labels has been drawn
+    var yLabelOffset=yLabel.node().getBBox().width
+    var yticksize=colculateTicksize(yAlign, yLabelOffset);
+
+    yLabel.call(yAxis.tickSize(yticksize))
+    yLabel
+        .attr("transform",function(){
+            if (yAlign=="right"){
+                return "translate("+(margin.left)+","+margin.top+")"
+            }
+            else return "translate("+(w-margin.right)+","+margin.top+")"
+            })
+
+    //identify 0 line if there is one
+    var originValue = 0;
+    var origin = plot.selectAll(".tick").filter(function(d, i) {
+            return d==originValue || d==yHighlight;
+        })
+    .classed(media+"origin",true);
+
+
+
+    function colculateTicksize(align, offset) {
+        if (align=="right") {
+            return w-margin.left-offset
+        }
+        else {return w-margin.right-offset}
+    }
+
+}
