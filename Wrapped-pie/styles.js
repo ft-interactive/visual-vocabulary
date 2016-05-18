cc8eda4a6899f762a46753d2c0241fe9f284d7ca
Bob Haslett <bob.haslett@ft.com>
2016-05-18 15:05:25 +0100

    Update to pie

--- Wrapped-pie/styles.js
+++ Wrapped-pie/styles.js
@@ -1,16 +1,17 @@
 
 var styleprint = {classes:
 [{class:"background{fill: none}"},
-{class:"chartholder{fill: #fff1e0}"},
+{class:"chartholder{fill: none}"},
 {class:"title{font-size:12px; fill: #000000; font-weight: 600;}"},
 {class:"subtitle{fill:#000000; font-size: 9.6px;}"},
 {class:"source{fill: #000000; font-size: 7.2px;}"},
 {class:"logo{fill: #dacfc5;}"},
 {class:"hat{fill:none; stroke-width:0.3px; stroke:#22190f;}"},
-{class:"labels{font-size:12px; fill: #000000; font-weight: 600;}"},
+{class:"labels{font-size:10px; fill: #ffffff; font-weight: 300;}"},
+{class:"arc{stroke-width: 1.0px; stroke: #ffffff}"},
 ],
 titleOffset:10,
-subOffset:15,
+subOffset:0,
 legendyOffset:0,
 linecolours:["#006a93","#ddb831","#c6d6cc","#55a2c7","#ad1c21","#486ba0"],
 fillcolours:["#006a93","#ddb831","#c6d6cc","#55a2c7","#ad1c21","#486ba0"]
@@ -18,15 +19,16 @@ fillcolours:["#006a93","#ddb831","#c6d6cc","#55a2c7","#ad1c21","#486ba0"]
 
 var styleweb = {classes:
 [{class:"background{fill: none}"},
-{class:"chartholder{fill: #fff1e0;}"},
+{class:"chartholder{fill: none;}"},
 {class:"title{font-size:21px; fill: #3b3d3b; font-weight: 100;}"},
 {class:"subtitle{font-size:14px; fill: #6b6e68; font-weight: 100;}"},
 {class:"source{fill: #6b6e68; font-size: 11.5px; font-weight: 100;}"},
 {class:"logo{fill: #dacfc5;}"},
-{class:"labels{font-size:12px; fill: #000000; font-weight: 600;}"}
+{class:"labels{font-size:12px; fill: #ffffff; font-weight: 600;}"},
+{class:"arc{stroke-width: 1.0px; stroke: #fff1e0}"},
 ],
 titleOffset:21,
-subOffset:20,
+subOffset:0,
 legendyOffset:0,
 linecolours:["#A5526A","#F19F9E","#D36969","#69A1AA","#66bfd4","#486ba0"],
 fillcolours:["#A5526A","#F19F9E","#D36969","#69A1AA","#66bfd4","#486ba0"]
@@ -34,15 +36,16 @@ fillcolours:["#A5526A","#F19F9E","#D36969","#69A1AA","#66bfd4","#486ba0"]
 
 var stylesoc = {classes:
 [{class:"background{fill: #212121;}"},
-{class:"chartholder{fill: #4d4d4f;}"},
+{class:"chartholder{fill: none;}"},
 {class:"title{font-size: 38px; fill: #ffffff; opacity: 0.95; font-weight: 400;}"},
 {class:"subtitle{font-size: 28px; fill: #ffffff; opacity: 0.70; font-weight: 100; line-height: 30px;}"},
 {class:"source{font-size: 25px; fill: #ffffff; opacity: 0.50; font-weight: 100;}"},
 {class:"logo{fill: #ffffff; opacity: 0.38;}"},
-{class:"labels{font-size: 38px; fill: #ffffff; opacity: 0.95; font-weight: 400;}"}
+{class:"labels{font-size: 38px; fill: #ffffff; opacity: 0.95; font-weight: 400;}"},
+{class:"arc{stroke-width: 2.0px; stroke: #212121}"},
 ],
 titleOffset:36,
-subOffset:30,
+subOffset:0,
 legendyOffset:0,
 linecolours:["#CC4759","#00B5B5","#E6B522","#437099","#FF8C8B","#CBFF7E","#8A8A8A"],
 fillcolours:["#CC4759","#00B5B5","#E6B522","#437099","#FF8C8B","#BCFF59","#8A8A8A"]
@@ -55,10 +58,11 @@ var stylevid = {classes:
 {class:"subtitle{font-size: 48; fill: #000000; font-weight: 600;}"},
 {class:"source{font-size: 44; fill: #000000; font-weight: 600;}"},
 {class:"logo{fill: #ffffff;}"},
-{class:"labels{font-size:12px; fill: #000000; font-weight: 600;}"}
+{class:"labels{font-size:12px; fill: #000000; font-weight: 600;}"},
+{class:"arc{stroke-1.0px;}"}
 ],
 titleOffset:68,
-subOffset:90,
+subOffset:0,
 legendyOffset:15,
 linecolours:["#b34b41","#5a8caf","#9baa6e","#eda45e","#9c5f87","#486ba0"],
 fillcolours:["#b34b41","#5a8caf","#9baa6e","#eda45e","#9c5f87","#486ba0"]
