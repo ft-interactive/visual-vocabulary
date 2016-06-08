943ddb830c8e373190d53cfad88e0a5f62c43ded
Bob Haslett <bob.haslett@ft.com>
2016-06-08 16:11:00 +0100

    Add yAxis and origin lines

--- wrapper-area/area.js
+++ wrapper-area/area.js
@@ -1,5 +1,5 @@
 
-function areaChart(data,stylename,media,plotpadding,legAlign,yAlign){
+function areaChart(data,stylename,media,plotpadding,legAlign,yAlign, yHighlight){
 
     var titleYoffset = d3.select("#"+media+"Title").node().getBBox().height
     var subtitleYoffset=d3.select("#"+media+"Subtitle").node().getBBox().height;
@@ -30,15 +30,27 @@ function areaChart(data,stylename,media,plotpadding,legAlign,yAlign){
     //Based on https://bl.ocks.org/mbostock/3885211
     console.log(data)
 
+    var xDomain = d3.extent(data, function(d) {return d.date;});
+    var yDomain;
+
+    //calculate range of y axis series data
+    var min=6;
+    var max=0;
+    data.forEach(function(d,i){
+        seriesNames.forEach(function(e){
+            if (d[e]){
+                min=Math.min(min,d[e]);
+                max=Math.max(max,d[e]);
+            }
+        });         
+    });
+    yDomain=[min,max];
+
     var xScale = d3.time.scale()
         .range([0, plotWidth]);
 
     var yScale = d3.scale.linear()
         .range([plotHeight, 0]);
-
-    var xAxis = d3.svg.axis()
-        .scale(xScale)
-        .orient("bottom");
 
     var area = d3.svg.area()
         .x(function(d) { return x(d.date); })
@@ -52,10 +64,49 @@ function areaChart(data,stylename,media,plotpadding,legAlign,yAlign){
         return {
             name: name,
             values: data.map(function(d) {
-                return {date: d.date, y: d[name] / 100};
+                return {date: d.date, y: d[name]};
             })
         };
     }));
+
     console.log(plotData)
+
+    var yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient(yAlign)
+
+    var yLabel=plot.append("g")
+    .attr("class",media+"yAxis")
+    .call(yAxis);
+
+    //calculate what the ticksize should be now that the text for the labels has been drawn
+    var yLabelOffset=yLabel.node().getBBox().width
+    //redraw the yScale on the page with the ticks on it and position it on the page
+    var yticksize=colculateTicksize(yAlign, yLabelOffset);
+    //console.log(yticksize);
+
+    yLabel.call(yAxis.tickSize(yticksize))
+    yLabel
+        .attr("transform",function(){
+            if (yAlign=="right"){
+                return "translate("+(margin.left)+","+margin.top+")"
+            }
+            else return "translate("+(w-margin.right)+","+margin.top+")"
+            })
+
+    //identify 0 line if there is one
+    var originValue = 0;
+    var origin = plot.selectAll(".tick").filter(function(d, i) {
+            return d==originValue || d==yHighlight;
+        })
+    .classed(media+"origin",true);
+
+
+    function colculateTicksize(align, offset) {
+        if (align=="right") {
+            return w-margin.left-offset
+        }
+        else {return w-margin.right-offset}
+    }
 
 }
