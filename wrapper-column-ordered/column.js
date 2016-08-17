8393f03223a1ff6b03a450217eb98b59443e8394
Bob Haslett <bob.haslett@ft.com>
2016-08-17 15:24:00 +0100

    change rangeRoundBands to rangeBands on column ordered

--- wrapper-column-ordered/column.js
+++ wrapper-column-ordered/column.js
@@ -76,7 +76,7 @@ function columnChart(data,stylename,media,yMin,yMax,plotpadding,legAlign,yHighli
     .classed(media+"origin",true);
 
     var xScale = d3.scale.ordinal()
-    .rangeRoundBands([0, plotWidth-yLabelOffset],.3);
+    .rangeBands([0, plotWidth-yLabelOffset],.3);
 
     var xAxis = d3.svg.axis()
     .scale(xScale)
