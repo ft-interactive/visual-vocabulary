9200fce01db9af45a6b04df916380718bf52d049
Bob Haslett <bob.haslett@ft.com>
2016-08-19 15:21:59 +0100

    Style change

--- column-grouped/styles.js
+++ column-grouped/styles.js
@@ -1,7 +1,7 @@
 
 var styleprint = {classes:
 [{class:"background{fill: none}"},
-{class:"chartholder{fill: #fff1e0}"},
+{class:"chartholder{fill: none}"},
 {class:"title{font-size:12px; fill: #000000; font-weight: 600;}"},
 {class:"subtitle{fill:#000000; font-size: 9.6px;}"},
 {class:"source{fill: #000000; font-size: 7.2px;}"},
@@ -23,8 +23,8 @@ fillcolours:["#006a93","#ddb831","#c6d6cc","#55a2c7","#ad1c21","#486ba0"]
 }
 
 var styleweb = {classes:
-[{class:"background{fill: #fff1e0}"},
-{class:"chartholder{fill: #ffffff;}"},
+[{class:"background{fill: none}"},
+{class:"chartholder{fill: none;}"},
 {class:"title{font-size:25.0px; fill: #3b3d3b; font-weight: 100;}"},
 {class:"subtitle{font-size:18.0px; fill: #6b6e68; font-weight: 100;}"},
 {class:"source{fill: #6b6e68; font-size: 14.0px; font-weight: 100;}"},
