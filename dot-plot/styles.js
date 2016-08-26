8d0dba888bf279cc7199d0fc9b4df5cabab8ef0a
Bob Haslett <bob.haslett@ft.com>
2016-08-26 12:32:03 +0100

    Small style change and folder clean up

--- dot-plot/styles.js
+++ dot-plot/styles.js
@@ -1,7 +1,7 @@
 
 var styleprint = {classes:
 [{class:"background{fill: none}"},
-{class:"chartholder{fill: #fff1e0}"},
+{class:"chartholder{fill: none}"},
 {class:"title{font-size:12px; fill: #000000; font-weight: 600;}"},
 {class:"subtitle{fill:#000000; font-size: 9.6px; font-weight: 400;}"},
 {class:"labels{font-size:9.6px; fill: #ffffff; font-weight: 400;}"},
@@ -34,7 +34,7 @@ fillcolours:["#006a93","#ddb831","#c6d6cc","#55a2c7","#ad1c21","#486ba0","#ddb83
 
 var styleweb = {classes:
 [{class:"background{fill: none}"},
-{class:"chartholder{fill: #fff1e0;}"},
+{class:"chartholder{fill: none;}"},
 {class:"title{font-size:25px; font-weight: 400; fill: #3b3d3b;}"},
 {class:"subtitle{font-size:18px; font-weight: 400; fill: #6b6e68;}"},
 {class:"source{fill: #6b6e68; font-size: 14.0px; font-weight: 400;}"},
