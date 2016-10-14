f90c128bd4cca90648d6436fef7446c79f775fd6
Bob Haslett <bob.haslett@ft.com>
2016-10-14 17:06:09 +0100

    Add x and y domains and test for min and max ranges

--- ZZ-scatterplot-UNFINISHED/scatterplot.js
+++ ZZ-scatterplot-UNFINISHED/scatterplot.js
@@ -28,6 +28,11 @@ function scatterplot(data,stylename,media,plotpadding,legAlign,yAlign){
     var xDomain = d3.extent(data, function(d) {return +d.x;});
     var yDomain = d3.extent(data, function(d) {return +d.y;});
 
+    xDomain[0]=Math.min(xMin,xDomain[0])
+    xDomain[1]=Math.max(xMax,xDomain[1])
+    yDomain[0]=Math.min(xMin,yDomain[0])
+    yDomain[1]=Math.max(xMax,yDomain[1])
+
     var plotData = d3.nest()
         .key(function(d) { return d.cat; })
         .entries(data);
