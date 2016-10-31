5182ed45bb0630d7fb8fda92286805d5864e6eb0
Bob Haslett <bob.haslett@ft.com>
2016-10-31 15:31:35 +0000

    Add heading and intro, set up data object and plot groups

---
+++ zzz-index unfinished/v2-data.js
@@ -0,0 +1,10 @@
+let data={"categories":[
+    {"category":"Deviation", "colour":"#fae596"},
+    {"category":"Correlation", "colour":"#e3e0cf"},
+    {"category":"Distribution", "colour":"#c2d4d8"},
+    {"category":"Change over Time", "colour":"#dbe9d8"},
+    {"category":"Magnitude", "colour":"#e1e0ec"},
+    {"category":"Spatial", "colour":"#dbc3d0"},
+    {"category":"Flow", "colour":"#e5efce"},
+]}
+
