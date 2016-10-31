2cec5518e18db8fd1f64cb2ecbc2dc2f3d7ffcd9
Bob Haslett <bob.haslett@ft.com>
2016-10-31 17:59:26 +0000

    End of day

--- zzz-index unfinished/v2-data.js
+++ zzz-index unfinished/v2-data.js
@@ -1,10 +1,13 @@
 let data={"categories":[
-    {"category":"Deviation", "colour":"#fae596"},
-    {"category":"Correlation", "colour":"#e3e0cf"},
-    {"category":"Distribution", "colour":"#c2d4d8"},
-    {"category":"Change over Time", "colour":"#dbe9d8"},
-    {"category":"Magnitude", "colour":"#e1e0ec"},
-    {"category":"Spatial", "colour":"#dbc3d0"},
-    {"category":"Flow", "colour":"#e5efce"},
+    {"category":"Deviation", "colour":"#fae596","description":"Emphasise variations (+/-) from a fixed reference point. Typically the reference point is zero but it can also be a target or a long-term average. Can also be used to show sentiment (positive/neutral/negative)"},
+    {"category":"Correlation", "colour":"#e3e0cf","description":"Show the relationship between two or more variables. Be mindful that, unless you tell them otherwise, many readers will assume the relationships you show them to be causal (i.e. one causes the other)."},
+    {"category":"Distribution", "colour":"#c2d4d8","description":"Emphasise variations (+/-) from a fixed reference point. Typically the reference point is zero but it can also be a target or a long-term average. Can also be used to show sentiment (positive/neutral/negative)"},
+    {"category":"Change v Time", "colour":"#dbe9d8","description":"Emphasise variations (+/-) from a fixed reference point. Typically the reference point is zero but it can also be a target or a long-term average. Can also be used to show sentiment (positive/neutral/negative)"},
+    {"category":"Magnitude", "colour":"#e1e0ec",
+"description":"Emphasise variations (+/-) from a fixed reference point. Typically the reference point is zero but it can also be a target or a long-term average. Can also be used to show sentiment (positive/neutral/negative)"},
+    {"category":"Spatial", "colour":"#dbc3d0",
+"description":"Emphasise variations (+/-) from a fixed reference point. Typically the reference point is zero but it can also be a target or a long-term average. Can also be used to show sentiment (positive/neutral/negative)"},
+    {"category":"Flow", "colour":"#e5efce",
+"description":"Emphasise variations (+/-) from a fixed reference point. Typically the reference point is zero but it can also be a target or a long-term average. Can also be used to show sentiment (positive/neutral/negative)"},
 ]}
 
